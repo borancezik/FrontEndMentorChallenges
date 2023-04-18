5a27d7de792997a1fbc51f87d7eb04fc9307ff14
Boran Cezik <borancezik@gmail.com>
2023-04-19 01:22:04 +0300

    changes

--- Challenge2/tailwind.config.js
+++ Challenge2/tailwind.config.js
@@ -8,6 +8,13 @@ module.exports = {
         128: "32rem",
         110: "28rem",
       },
+      colors: {
+        "dark-cyan": "hsl(158, 36%, 37%)",
+        //prettier-ignore
+        "cream": "hsl(30, 38%, 92%)",
+        "very-dark-blue": "hsl(212, 21%, 14%)",
+        "dark-grayish-blue": "hsl(228, 12%, 48%)",
+      },
       height: {
         128: "32rem",
         200: "48rem",
